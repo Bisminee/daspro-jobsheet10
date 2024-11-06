68f3cee5d9e2b70897cb635b7155925dea209ed7
Bisminee <fiksilmiah@gmail.com>
2024-11-06 09:09:39 +0700

    Arrad 2 Dimensi

--- BioskopWithScanner07.java
+++ BioskopWithScanner07.java
@@ -3,26 +3,82 @@ public class BioskopWithScanner07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String penonton[][] = new String[4][2];
-        int baris, kolom;
+        int baris, kolom, pilihan;
         String nama, next;
 
         while(true){
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+
+            for (int i = 0; i < penonton.length; i++) {
+                for (int j = 0; j < penonton[i].length; j++) {
+                    if (penonton[i][j] == null) {
+                        penonton[i][j] = "***";
+                    }
+                }
+            }
+
+            System.out.println("SELAMAT DATANG DI BIOSKOP KOPKOP");
+            System.out.println("1. Pesan Tempat Duduk");
+            System.out.println("2. Daftar Penonton");
+            System.out.println("0. Keluar");
+            System.out.print("Masukkan pilihan dengan angka: ");
+            pilihan = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris - 1][kolom - 1] = nama;
+            switch (pilihan) {
+                
+                case 0:
+                break;
+                case 1:
+                    while(true){
+                        System.out.print("Masukkan nama: ");
+                        nama = sc.nextLine();
+                        System.out.print("Masukkan baris: ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        kolom = sc.nextInt();
+                        sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                        if(penonton[(baris-1)][(kolom-1)] != "***"){
+                            System.out.println("\nKURSI SUDAH TERISI!");
+                            System.out.println("MOHON LIHAT DAFTAR PENONTON UNTUK MENGETAHUI KURSI KOSONG!\n");
+                            break;
+                        }
 
-            if(next.equalsIgnoreCase("n")){
+                        if(baris > penonton.length || kolom > penonton[0].length || baris <= 0 || kolom <= 0){
+                            System.out.println("\nMASUKKAN KURSI YANG TERSEDIA!");
+                            System.out.println("MOHON LIHAT DAFTAR PENONTON UNTUK MENGETAHUI KURSI KOSONG!\n");
+                            break;
+                        }
+
+                        penonton[(baris - 1)][(kolom - 1)] = nama;
+
+
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        next = sc.nextLine();
+
+                        if(next.equalsIgnoreCase("n")){
+                            break;
+                        }
+                    }
+                break;
+                case 2:
+                    System.out.println();
+                    System.out.println("Daftar Penonton");
+                    for (int i = 0; i < penonton.length; i++) {
+                        System.out.println("Penonton baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+                    }
+                    System.out.println();
+                break;
+                default:
+                    System.out.println("MASUKKAN PILIHAN YANG TERSEDIA SAJA!!!");
+            }
+
+            if(pilihan == 0){
                 break;
             }
+            
+            
         }
+        System.out.println("TERIMA KASIH");
     }
 }
