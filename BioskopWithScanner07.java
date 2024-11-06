876c368cefc495d52e03eb7640679266191350ba
Bisminee <fiksilmiah@gmail.com>
2024-11-06 08:17:12 +0700

    Arrad 2 Dimensi

--- BioskopWithScanner07.java
+++ BioskopWithScanner07.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class BioskopWithScanner {
+public class BioskopWithScanner07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String penonton[][] = new String[4][2];
