e6a6f0a6b384925e708a832159878cf7a692ec49
Bisminee <fiksilmiah@gmail.com>
2024-11-06 10:43:15 +0700

    Array 2 Dimensi

--- SIAKAD07.java
+++ SIAKAD07.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import jdk.jshell.spi.ExecutionControl;
 public class SIAKAD07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
